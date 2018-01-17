21d4085a98058a51e32a5b6c340eba6bd7e2e682
yeweijie <354977983@qq.com>
2018-01-17 14:45:40 +0800

    '第一次提交项目'

---
+++ src/components/home/index/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+  <div class="content">
+    <div class="search">
+        <input type="text" placeholder="请输入搜索内容" />
+    </div>
+        <div class="banner">
+            <!--<img src="static/img/banner.png"/>-->
+        </div>
+  </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'home-header'
+}
+</script>
+
+<style scoped>
+.header{
+    width: 100%;
+    height: 80px;
+    background-image: linear-gradient(90deg,#0af,#0085ff);
+}
+
+
+</style>
---
+++ src/pages/home/SubPage.vue
@@ -0,0 +1,25 @@
+<template>
+<sub-page class="subpage" :id="pageId">
+
+</sub-page>
+</template>
+
+<script>
+  import SubPage from '../../common/SubPage.vue'
+  export default{
+      components:{
+          [SubPage.name]:SubPage
+      }
+  }
+</script>
+
+<style>
+  .subpage{
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0px;
+    z-index: 5;
+  }
+</style>
